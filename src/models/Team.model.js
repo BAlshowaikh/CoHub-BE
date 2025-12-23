1eee25c8b61b31ad5155de0656d8b71f3cb41126
tulefat <tolefat1989@gmail.com>
2025-12-23 09:50:58 +0300

    Add team model

---
+++ src/models/Team.model.js
@@ -0,0 +1,9 @@
+const { Schema, default: mongoose } = require('mongoose')
+const teamSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    members: [{ type:mongoose.Schema.Types.ObjectId,ref:"User" }]},
+    { timestamps: true }
+)
+module.exports = teamSchema
