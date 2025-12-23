f4953dc0ad5b909123aec920754bd02eac319b4c
tulefat <tolefat1989@gmail.com>
2025-12-23 19:56:38 +0300

    add middleware to the route and fix error in teams controller

--- src/routes/teams.routes.js
+++ src/routes/teams.routes.js
@@ -1,13 +1,35 @@
 const router = require("express").Router()
 
 const teamCtrl = require("../controllers/teams.controller")
+const middleware = require('../middleware')
 
-router.get("/",teamCtrl.team_index_get);
-router.post("/",teamCtrl.team_create_post);
-router.get("/new",teamCtrl.team_create_get);
-router.get("/:teamId",teamCtrl.team_show_get);
-router.get("/:teamId/edit",teamCtrl.team_edit_get);
-router.put("/:teamId",teamCtrl.team_update_put);
-router.delete("/:teamId",teamCtrl.team_delete_delete);
+router.get("/",
+  middleware.stripToken,
+  middleware.verifyToken,
+  teamCtrl.team_index_get);
+router.post("/",
+  middleware.stripToken,
+  middleware.verifyToken,
+  teamCtrl.team_create_post);
+router.get("/new",
+    middleware.stripToken,
+  middleware.verifyToken,
+  teamCtrl.team_create_get);
+router.get("/:teamId",
+  middleware.stripToken,
+  middleware.verifyToken,
+  teamCtrl.team_show_get);
+router.get("/:teamId/edit",
+    middleware.stripToken,
+  middleware.verifyToken,
+  teamCtrl.team_edit_get);
+router.put("/:teamId",
+    middleware.stripToken,
+  middleware.verifyToken,
+  teamCtrl.team_update_put);
+router.delete("/:teamId",
+    middleware.stripToken,
+  middleware.verifyToken,
+  teamCtrl.team_delete_delete);
 
 module.exports = router
