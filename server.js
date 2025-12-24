ba766e2fc4595330d1202d335c6b9f77245122e6
BAlshowaikh <batoolalshowaikh04@outlook.com>
2025-12-24 18:07:56 +0300

    Added routers:

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ dotenv.config()
 const express = require("express")
 const app = express()
 
+// --------------- Required Routes ------------------
+const taskRoute = require("./src/routes/tasks.routes")
+const projectRoute = require("./src/routes/projects.routes")
+
+// --------------- Use the routes ---------------
+app.use("/task", taskRoute)
+app.use("/project", projectRoute)
+
 // ----- DB Connection ----
 const mongoose = require("./src/config/db")
 const port = process.env.PORT ? process.env.PORT : 3000
