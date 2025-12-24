3242ac0091226b2647c95c9f832341dda25e35a7
tulefat <tolefat1989@gmail.com>
2025-12-24 10:34:45 +0300

    Add code related tp team and auth in server.js

--- server.js
+++ server.js
@@ -5,6 +5,20 @@ dotenv.config()
 const express = require("express")
 const app = express()
 
+const logger = require('morgan')
+const cors = require('cors')
+
+const AuthRouter = require('./src/routes/auth.routes')
+const TeamRouter = require('./src/routes/teams.routes')
+
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/auth', AuthRouter)
+app.use('/teams', TeamRouter)
+
 // ----- DB Connection ----
 const mongoose = require("./src/config/db")
 const port = process.env.PORT ? process.env.PORT : 3000
