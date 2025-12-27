84130dfb83dd3b1dc72b9687fff5ce8ef0b79f7c
tulefat <tolefat1989@gmail.com>
2025-12-27 16:50:39 +0300

    modify User model

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose')   
-const { Schema } = require('mongoose')
+const mongoose = require('mongoose')
+const { Schema } = mongoose
 
 const userSchema = new Schema(
   {
@@ -7,10 +7,10 @@ const userSchema = new Schema(
     email: { type: String, required: true },
     passwordDigest: { type: String, required: true },
     fullname: { type: String, required: true },
-    user_role: { type: String, enum:["PM","Employee","Manager"],required: true },
-        department: { type: String, enum:["IT","HR","Marketing"],required: true }
-  }, { timestamps: true }
+    user_role: { type: String, enum:["PM","Employee","Manager"], required: true },
+    department: { type: String, enum:["IT","HR","Marketing"], required: true }
+  },
+  { timestamps: true }
 )
 
 module.exports = mongoose.model('User', userSchema)
-module.exports = userSchema
