c02d78e33aea7612ef78fbe3eabdae8a42b0a5f7
[200~hassanali121212w-svg~ <hassanali121212w@gmail.com>
2025-12-28 20:16:38 +0300

    check and finalise project CRUD

--- server.js
+++ server.js
@@ -5,14 +5,21 @@ dotenv.config()
 const express = require("express")
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
+const ProjectRouter = require("./src/routes/projects.routes")
+app.use("/project", ProjectRouter)
 
 // ----- DB Connection ----
 const mongoose = require("./src/config/db")
 const port = process.env.PORT ? process.env.PORT : 3000
 
+app.use("/", (req, res) => {
+  res.send("connected")
+})
+
 // ----------- Start the BE server ----------------
 app.listen(port, () => {
   console.log(`App is listening in port ${process.env.PORT}`)
 })
-
